e22fe36bfe838b819126a9fc26829fd186a1bb4e
Juraj Noge <juraj.noge@productboard.com>
2025-12-16 20:20:53 +0100

    Day 5 - faster

--- src/main/kotlin/day5.kt
+++ src/main/kotlin/day5.kt
@@ -3,28 +3,16 @@ import utils.readInputBlock
 /** [https://adventofcode.com/2018/day/5] */
 class Reactions : AdventOfCodeTask {
     override fun run(part2: Boolean): Any {
-        val input = readInputBlock("5.txt").toMutableList()
+        val polymer = readInputBlock("5.txt")
+        val pattern =
+            ('a'..'z').flatMap { setOf("$it${it.uppercase()}", "${it.uppercase()}$it") }.joinToString("|").toRegex()
+
+        fun react(polymer: String): Int =
+            polymer.replace(pattern, "").let { next -> if (next.length == polymer.length) next.length else react(next) }
 
         return if (part2) {
-            ('A'..'Z').zip('a'..'z')
-                .minOf { (l, u) -> react(input.filter { it != l && it != u }.toMutableList()) }
-        } else react(input)
-    }
-
-    private fun react(input: MutableList<Char>): Int {
-        while (true) {
-            input.withIndex().zipWithNext()
-                .firstOrNull { (a, b) ->
-                    a.value.equals(b.value, ignoreCase = true) && !a.value.equals(
-                        b.value,
-                        ignoreCase = false
-                    )
-                }
-                ?.also { (a, _) -> input.removeAt(a.index); input.removeAt(a.index) }
-                ?: break
-        }
-
-        return input.size
+            ('a'..'z').minOf { react(polymer.replace("$it|${it.uppercase()}".toRegex(), "")) }
+        } else react(polymer)
     }
 }
 
